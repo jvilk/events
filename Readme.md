086b9130fff897bad1f9022cb041c81bff4ac7fd
John Vilk <jvilk@cs.umass.edu>
2016-07-27 11:42:18 -0500

    Make it ES6-compatible.

--- Readme.md
+++ Readme.md
@@ -1,6 +1,23 @@
 # events [![Build Status](https://travis-ci.org/Gozala/events.png?branch=master)](https://travis-ci.org/Gozala/events)
 
-Node's event emitter for all engines.
+Node's event emitter for all engines. Modified to include an ES6 version.
+
+## Breaking Changes ##
+
+ES6 modules do not let you export an object as the module. Thus,
+the following will not work if you are using the non-ES6 version:
+
+```javascript
+var EventEmitter = require('events');
+var emitter = new EventEmitter();
+```
+
+Instead, do the following:
+
+```javascript
+var EventEmitter = require('events').EventEmitter;
+var emitter = new EventEmitter();
+```
 
 ## Install ##
 
--- events-es6.js
+++ events-es6.js
@@ -1,13 +1,3 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
-
-exports.EventEmitter = EventEmitter;
-exports.listenerCount = listenerCount;
 // Copyright Joyent, Inc. and other Node contributors.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
@@ -29,14 +19,13 @@ exports.listenerCount = listenerCount;
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-function EventEmitter() {
+export function EventEmitter() {
   this._events = this._events || {};
   this._maxListeners = this._maxListeners || undefined;
 }
-exports.default = EventEmitter;
+export default EventEmitter;
 
 // Backwards-compat with node 0.10.x
-
 EventEmitter.EventEmitter = EventEmitter;
 
 EventEmitter.prototype._events = undefined;
@@ -45,24 +34,27 @@ EventEmitter.prototype._maxListeners = undefined;
 // By default EventEmitters will print a warning if more than 10 listeners are
 // added to it. This is a useful default which helps finding memory leaks.
 EventEmitter.defaultMaxListeners = 10;
-var defaultMaxListeners = exports.defaultMaxListeners = 10;
+export const defaultMaxListeners = 10;
 
 // Obviously not all Emitters should be limited to 10. This function allows
 // that to be increased. Set to zero for unlimited.
-EventEmitter.prototype.setMaxListeners = function (n) {
-  if (!isNumber(n) || n < 0 || isNaN(n)) throw TypeError('n must be a positive number');
+EventEmitter.prototype.setMaxListeners = function(n) {
+  if (!isNumber(n) || n < 0 || isNaN(n))
+    throw TypeError('n must be a positive number');
   this._maxListeners = n;
   return this;
 };
 
-EventEmitter.prototype.emit = function (type) {
+EventEmitter.prototype.emit = function(type) {
   var er, handler, len, args, i, listeners;
 
-  if (!this._events) this._events = {};
+  if (!this._events)
+    this._events = {};
 
   // If there is no 'error' event listener then throw.
   if (type === 'error') {
-    if (!this._events.error || isObject(this._events.error) && !this._events.error.length) {
+    if (!this._events.error ||
+        (isObject(this._events.error) && !this._events.error.length)) {
       er = arguments[1];
       if (er instanceof Error) {
         throw er; // Unhandled 'error' event
@@ -77,7 +69,8 @@ EventEmitter.prototype.emit = function (type) {
 
   handler = this._events[type];
 
-  if (isUndefined(handler)) return false;
+  if (isUndefined(handler))
+    return false;
 
   if (isFunction(handler)) {
     switch (arguments.length) {
@@ -100,30 +93,36 @@ EventEmitter.prototype.emit = function (type) {
     args = Array.prototype.slice.call(arguments, 1);
     listeners = handler.slice();
     len = listeners.length;
-    for (i = 0; i < len; i++) {
+    for (i = 0; i < len; i++)
       listeners[i].apply(this, args);
-    }
   }
 
   return true;
 };
 
-EventEmitter.prototype.addListener = function (type, listener) {
+EventEmitter.prototype.addListener = function(type, listener) {
   var m;
 
-  if (!isFunction(listener)) throw TypeError('listener must be a function');
+  if (!isFunction(listener))
+    throw TypeError('listener must be a function');
 
-  if (!this._events) this._events = {};
+  if (!this._events)
+    this._events = {};
 
   // To avoid recursion in the case that type === "newListener"! Before
   // adding it to the listeners, first emit "newListener".
-  if (this._events.newListener) this.emit('newListener', type, isFunction(listener.listener) ? listener.listener : listener);
+  if (this._events.newListener)
+    this.emit('newListener', type,
+              isFunction(listener.listener) ?
+              listener.listener : listener);
 
   if (!this._events[type])
     // Optimize the case of one listener. Don't need the extra array object.
-    this._events[type] = listener;else if (isObject(this._events[type]))
+    this._events[type] = listener;
+  else if (isObject(this._events[type]))
     // If we've already got an array, just append.
-    this._events[type].push(listener);else
+    this._events[type].push(listener);
+  else
     // Adding the second element, need to change to array.
     this._events[type] = [this._events[type], listener];
 
@@ -137,7 +136,10 @@ EventEmitter.prototype.addListener = function (type, listener) {
 
     if (m && m > 0 && this._events[type].length > m) {
       this._events[type].warned = true;
-      console.error('(node) warning: possible EventEmitter memory ' + 'leak detected. %d listeners added. ' + 'Use emitter.setMaxListeners() to increase limit.', this._events[type].length);
+      console.error('(node) warning: possible EventEmitter memory ' +
+                    'leak detected. %d listeners added. ' +
+                    'Use emitter.setMaxListeners() to increase limit.',
+                    this._events[type].length);
       if (typeof console.trace === 'function') {
         // not supported in IE 10
         console.trace();
@@ -150,8 +152,9 @@ EventEmitter.prototype.addListener = function (type, listener) {
 
 EventEmitter.prototype.on = EventEmitter.prototype.addListener;
 
-EventEmitter.prototype.once = function (type, listener) {
-  if (!isFunction(listener)) throw TypeError('listener must be a function');
+EventEmitter.prototype.once = function(type, listener) {
+  if (!isFunction(listener))
+    throw TypeError('listener must be a function');
 
   var fired = false;
 
@@ -171,29 +174,36 @@ EventEmitter.prototype.once = function (type, listener) {
 };
 
 // emits a 'removeListener' event iff the listener was removed
-EventEmitter.prototype.removeListener = function (type, listener) {
+EventEmitter.prototype.removeListener = function(type, listener) {
   var list, position, length, i;
 
-  if (!isFunction(listener)) throw TypeError('listener must be a function');
+  if (!isFunction(listener))
+    throw TypeError('listener must be a function');
 
-  if (!this._events || !this._events[type]) return this;
+  if (!this._events || !this._events[type])
+    return this;
 
   list = this._events[type];
   length = list.length;
   position = -1;
 
-  if (list === listener || isFunction(list.listener) && list.listener === listener) {
+  if (list === listener ||
+      (isFunction(list.listener) && list.listener === listener)) {
     delete this._events[type];
-    if (this._events.removeListener) this.emit('removeListener', type, listener);
+    if (this._events.removeListener)
+      this.emit('removeListener', type, listener);
+
   } else if (isObject(list)) {
     for (i = length; i-- > 0;) {
-      if (list[i] === listener || list[i].listener && list[i].listener === listener) {
+      if (list[i] === listener ||
+          (list[i].listener && list[i].listener === listener)) {
         position = i;
         break;
       }
     }
 
-    if (position < 0) return this;
+    if (position < 0)
+      return this;
 
     if (list.length === 1) {
       list.length = 0;
@@ -202,20 +212,25 @@ EventEmitter.prototype.removeListener = function (type, listener) {
       list.splice(position, 1);
     }
 
-    if (this._events.removeListener) this.emit('removeListener', type, listener);
+    if (this._events.removeListener)
+      this.emit('removeListener', type, listener);
   }
 
   return this;
 };
 
-EventEmitter.prototype.removeAllListeners = function (type) {
+EventEmitter.prototype.removeAllListeners = function(type) {
   var key, listeners;
 
-  if (!this._events) return this;
+  if (!this._events)
+    return this;
 
   // not listening for removeListener, no need to emit
   if (!this._events.removeListener) {
-    if (arguments.length === 0) this._events = {};else if (this._events[type]) delete this._events[type];
+    if (arguments.length === 0)
+      this._events = {};
+    else if (this._events[type])
+      delete this._events[type];
     return this;
   }
 
@@ -236,31 +251,38 @@ EventEmitter.prototype.removeAllListeners = function (type) {
     this.removeListener(type, listeners);
   } else if (listeners) {
     // LIFO order
-    while (listeners.length) {
+    while (listeners.length)
       this.removeListener(type, listeners[listeners.length - 1]);
-    }
   }
   delete this._events[type];
 
   return this;
 };
 
-EventEmitter.prototype.listeners = function (type) {
+EventEmitter.prototype.listeners = function(type) {
   var ret;
-  if (!this._events || !this._events[type]) ret = [];else if (isFunction(this._events[type])) ret = [this._events[type]];else ret = this._events[type].slice();
+  if (!this._events || !this._events[type])
+    ret = [];
+  else if (isFunction(this._events[type]))
+    ret = [this._events[type]];
+  else
+    ret = this._events[type].slice();
   return ret;
 };
 
-EventEmitter.prototype.listenerCount = function (type) {
+EventEmitter.prototype.listenerCount = function(type) {
   if (this._events) {
     var evlistener = this._events[type];
 
-    if (isFunction(evlistener)) return 1;else if (evlistener) return evlistener.length;
+    if (isFunction(evlistener))
+      return 1;
+    else if (evlistener)
+      return evlistener.length;
   }
   return 0;
 };
 
-function listenerCount(emitter, type) {
+export function listenerCount(emitter, type) {
   return emitter.listenerCount(type);
 }
 EventEmitter.listenerCount = listenerCount;
@@ -274,7 +296,7 @@ function isNumber(arg) {
 }
 
 function isObject(arg) {
-  return (typeof arg === 'undefined' ? 'undefined' : _typeof(arg)) === 'object' && arg !== null;
+  return typeof arg === 'object' && arg !== null;
 }
 
 function isUndefined(arg) {
